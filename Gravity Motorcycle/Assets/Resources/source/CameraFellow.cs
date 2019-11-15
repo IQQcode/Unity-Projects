172284ef7ea2a2414477c23589899bb66deac78b
Mr.Q <2726109782@qq.com>
2019-11-15 23:54:08 +0800

    Gravity Motorcycle

---
+++ Gravity Motorcycle/Assets/Resources/source/CameraFellow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * 摄像头的视角随着相机的变化而变化
+  */
+public class CameraFellow : MonoBehaviour
+{
+    public Transform target;
+    Vector3 carmera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        carmera = target.position;
+        carmera.z = -10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        carmera = target.position;
+        carmera.z = -10;
+        transform.position = carmera;
+    }
+}
---
+++ Gravity Motorcycle/Assets/Resources/source/MotoCtrl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotoCtrl : MonoBehaviour
+{
+    public WheelJoint2D right; //motor right wheel
+    public WheelJoint2D left;  //motor left wheel
+
+    //摩托车速度
+    [SerializeField] float speed = 2200f;
+    //摩托车旋转
+    [SerializeField] float rotate = 800f;
+
+    Rigidbody2D rg; 
+
+    //声明motor
+    JointMotor2D motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor.maxMotorTorque = 10000;
+        rg = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //通过键盘来控制摩托车每一帧运行的方向
+        motor.motorSpeed = -speed * Input.GetAxis("Vertical");
+        right.motor = motor;
+        left.motor = motor;
+
+        //deltaTime 避免每一帧调用
+        rg.AddTorque( -rotate * Input.GetAxis("Horizontal") * Time.deltaTime);
+
+    } 
+}
